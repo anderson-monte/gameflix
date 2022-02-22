b5ef07556805606669b1eb1cacab444e63721795
Anderson Monte <andersonsmont@gmail.com>
2022-02-22 06:09:10 -0300

    adcionando youtube

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function carregarYouTube () {
+    document.getElementById("youtube").innerHTML = '<iframe width="500" height="315" src="https://www.youtube.com/embed/ztwwQByMmmE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+    console.log("cliquei");
+}
+
